285c24ec2d952f950eca334fd98a228eb62a9482
Doranous <andrei_doran@yahoo.com>
2025-01-11 01:50:40 +0200

    aa

--- Assets/Scripts/Player/PlayerRespawn.cs
+++ Assets/Scripts/Player/PlayerRespawn.cs
@@ -2,22 +2,30 @@ using UnityEngine;
 
 public class PlayerRespawn : MonoBehaviour
 {
-    //[SerializeField] private AudioClip checkpoint;
+    [SerializeField] private AudioClip checkpoint;
     private Transform currentCheckpoint;
     private Health playerHealth;
+    private UIManager uiManager;
 
     private void Awake()
     {
         playerHealth = GetComponent<Health>();
+        uiManager = FindObjectOfType<UIManager>();
     }
 
-    public void Respawn()
+    public void RespawnCheck()
     {
+        if (currentCheckpoint == null)
+        {
+            uiManager.GameOver();
+            return;
+        }
+
         playerHealth.Respawn(); //Restore player health and reset animation
         transform.position = currentCheckpoint.position; //Move player to checkpoint location
 
         //Move the camera to the checkpoint's room
-        //Camera.main.GetComponent<MovementController>().MoveToNewRoom(currentCheckpoint.parent);
+        //Camera.main.GetComponent<CameraController>().MoveToNewRoom(currentCheckpoint.parent);
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -26,7 +34,7 @@ public class PlayerRespawn : MonoBehaviour
             currentCheckpoint = collision.transform;
             //SoundManager.instance.PlaySound(checkpoint);
             collision.GetComponent<Collider2D>().enabled = false;
-            collision.GetComponent<Animator>().SetTrigger("activate");
+            collision.GetComponent<Animator>().SetTrigger("appear");
         }
     }
 }
